06c499bc40fad8740a99ccf5e50d5e6a3ced362c
Maik Kempe <mkempe@bitaculous.com>
2017-11-09 23:08:16 +0100

    [➠] Refactored TypoScripts.

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -1,4 +1,49 @@
+# -------------------------
+# | T3v Cookies Constants |
+# -------------------------
+
+# t3v {
+#   cookies {
+#     gridElements {
+#       noteGridElement {
+#         container = default
+#       }
+#     }
+#   }
+# }
+
+# === Plugin Constants ===
+
+plugin {
+  tx_t3vcookies {
+    persistence {
+      # cat=plugin/tx_t3vcookies/persistence; type=boolean; label=Enables the automatic cache clearing when changing data sets
+      enableAutomaticCacheClearing = 1
+
+      # cat=plugin/tx_t3vcookies/persistence; type=int+; label=The PID of the storage
+      storagePid = 0
+
+      # cat=plugin/tx_t3vcookies/persistence; type=boolean; label=Updates reference index to ensure data integrity
+      updateReferenceIndex = 1
+    }
+
+    settings {
+      # ...
+    }
+
+    view {
+      # cat=plugin/tx_t3vcookies/view; type=string; label=The default path to the layouts
+      layoutRootPath = EXT:t3v_cookies/Resources/Private/Layouts/
+
+      # cat=plugin/tx_t3vcookies/view; type=string; label=The default path to the templates
+      templateRootPath = EXT:t3v_cookies/Resources/Private/Templates/
+
+      # cat=plugin/tx_t3vcookies/view; type=string; label=The default path to the partials
+      partialRootPath = EXT:t3v_cookies/Resources/Private/Partials/
+    }
+  }
+}
+
 # === Includes ===
 
-<INCLUDE_TYPOSCRIPT: source="FILE: ./Vendor/constants.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE: ./GridElements/constants.ts">
--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,4 +1,39 @@
+# -----------------------------
+# | T3v Cookies Configuration |
+# -----------------------------
+
+# === Plugin Configuration ===
+
+plugin {
+  tx_t3vcookies {
+    persistence {
+      enableAutomaticCacheClearing = {$plugin.tx_t3vcookies.persistence.enableAutomaticCacheClearing}
+
+      storagePid = {$plugin.tx_t3vcookies.persistence.storagePid}
+
+      updateReferenceIndex = {$plugin.tx_t3vcookies.persistence.updateReferenceIndex}
+    }
+
+    settings {
+      # ...
+    }
+
+    view {
+      layoutRootPaths {
+        0 = {$plugin.tx_t3vcookies.view.layoutRootPath}
+      }
+
+      templateRootPaths {
+        0 = {$plugin.tx_t3vcookies.view.templateRootPath}
+      }
+
+      partialRootPaths {
+        0 = {$plugin.tx_t3vcookies.view.partialRootPath}
+      }
+    }
+  }
+}
+
 # === Includes ===
 
-<INCLUDE_TYPOSCRIPT: source="FILE: ./Vendor/setup.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE: ./GridElements/setup.ts">
