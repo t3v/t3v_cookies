098a892586b20c317858b3b111b6270172068103
Maik Kempe <mkempe@bitaculous.com>
2017-11-18 16:48:51 +0100

    [➠] Updated comments [ci skip].

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -35,13 +35,13 @@ plugin {
     }
 
     view {
-      # cat=plugin/tx_t3vcookies/view; type=string; label=The default path to the layouts
+      # cat=plugin/tx_t3vcookies/view; type=string; label=The path where the layouts are stored
       layoutRootPath = EXT:t3v_cookies/Resources/Private/Layouts/
 
-      # cat=plugin/tx_t3vcookies/view; type=string; label=The default path to the templates
+      # cat=plugin/tx_t3vcookies/view; type=string; label=The path where the templates are stored
       templateRootPath = EXT:t3v_cookies/Resources/Private/Templates/
 
-      # cat=plugin/tx_t3vcookies/view; type=string; label=The default path to the partials
+      # cat=plugin/tx_t3vcookies/view; type=string; label=The path where the partials are stored
       partialRootPath = EXT:t3v_cookies/Resources/Private/Partials/
     }
   }
